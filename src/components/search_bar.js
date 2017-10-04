7aed011205501db110e8ef64c00d0e80cc57b57a
Brittny Lapierre <lapierre@ca.ibm.com>
2017-10-04 16:37:50 -0400

    imple project started

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react'; //needed for jsx
+
+const SearchBar = () => {
+    return <input/>
+}
+
+export default SearchBar; //export the component
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';//relative path for own component
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const YT_API_KEY = 'AIzaSyBo0sjKytv20IyoQOFsewOfAN94VE-M_MU';
+//Create a new component, this component should produce some html
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//const es2016 -> it's a constant 
+const App = () => {
+    return (
+        <div> 
+            <SearchBar/> 
+        </div>
+    );//this is jsx
+}
+//now you can do <App></App> or <App/> to create an instance of app
+//take this component's generated html and put it in the DOM
+ReactDOM.render(<App/>, document.querySelector('.app')); //instantiate your component then send it to the dom in the target container, the root node of the entire applicaton
